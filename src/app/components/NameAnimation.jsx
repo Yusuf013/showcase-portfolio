242d2e982770d4d6821fc7356f451927b559876b
Yusuf013 <146951398+Yusuf013@users.noreply.github.com>
2024-12-26 22:32:03 +0100

    landingpage name-component first version

---
+++ src/app/components/NameAnimation.jsx
@@ -0,0 +1,43 @@
+'use client'
+
+import React, { useState, useEffect } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+
+const names = [
+  "{Yusuf Taşçı}",
+  "{ ユスフタスチ}",
+  "{يوسف تاشجي}",
+  "{Юсуф Тасчи}",
+  "{유수프 타스치}",
+  "{Γιουσούφ Τάσκι}",
+  "{Yusuf Taşçı}",
+]
+
+export default function NameAnimation() {
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % names.length)
+    }, 2000) // Verander de naam elke 2 seconden
+
+    return () => clearInterval(interval)
+  }, [])
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={currentIndex}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ duration: 0.5 }}
+          className="text-8xl font-bold text-[#F47519]"
+        >
+          {names[currentIndex]}
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  )
+}

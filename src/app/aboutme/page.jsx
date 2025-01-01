d7cddba6fe213b048215759606fa173946b7fc53
Yusuf013 <146951398+Yusuf013@users.noreply.github.com>
2025-01-01 22:17:54 +0100

    projects pagina bijna af

---
+++ src/app/aboutme/page.jsx
@@ -0,0 +1,7 @@
+import FloatingText from '../components/FloatingText';
+
+export default function AboutMe() {
+  return (
+    <FloatingText />
+  );
+}
--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,103 +1,179 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Projects() {
+  const [hoveredProject, setHoveredProject] = useState("Kies project");
+  const router = useRouter(); // Gebruik de router voor navigatie
+
+  const handlePolygonClick = (path) => {
+    router.push(path); // Navigeer naar de opgegeven route
+  };
+
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="flex flex-col items-center space-y-4">
-        {/* Container voor de polygonen */}
-        <div className="relative w-[200px] h-[200px] mt-[325px]">
-          {/* Originele polygon */}
-          <div
-            className="absolute w-full h-full mask-polygon"
-            style={{
-              transform: " scale(1.4)",
-              backgroundImage: "url('/images/mcdonald.svg')",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              WebkitMaskImage: "url('/polygons/Polygon1.svg')",
-              WebkitMaskSize: "contain", // Mask schaal aanpassen
-              WebkitMaskRepeat: "no-repeat",
-              maskImage: "url('/polygons/Polygon1.svg')",
-              maskSize: "contain",
-              maskRepeat: "no-repeat",
-              overflow: "visible", // Mask niet afsnijden
-            }}
-          ></div>
+    <div className="flex items-center justify-center h-screen relative">
+      {/* Tekst in het midden van de pagina met transitie */}
+      <div
+        className="absolute top-[45%] left-45% transform -translate-x-1/2 -translate-y-1/2 text-xl font-bold text-white transition-all duration-500 ease-in-out opacity-100"
+        style={{
+          opacity: hoveredProject === "Kies project" ? 0.8 : 1,
+          transform: hoveredProject === "Kies project" ? "scale(1)" : "scale(1.1)",
+        }}
+      >
+        {hoveredProject}
+      </div>
 
-          {/* Nieuwe polygon - Rechtsboven, 55 graden gedraaid */}
-          <div
-            className="absolute w-full h-full mask-polygon"
-            style={{
-              transform: "translate(50%, -50%) rotate(-73deg) scale(1.4)",
-              top: "-10%",
-              right: "-40%",
-              backgroundImage: "url('/images/Upendo.png')",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              WebkitMaskImage: "url('/polygons/Polygon1.svg')",
-              WebkitMaskSize: "contain",
-              WebkitMaskRepeat: "no-repeat",
-              maskImage: "url('/polygons/Polygon1.svg')",
-              maskSize: "contain",
-              maskRepeat: "no-repeat",
-            }}
-          ></div>
+      {/* Container voor de polygonen */}
+      <div
+        className="relative w-[200px] h-[200px] mt-[400px] perspective-[1000px]"
+        style={{ perspective: "1000px" }}
+      >
+        {/* Originele polygon */}
+        <div
+          className="absolute w-full h-full mask-polygon transition-transform duration-300 cursor-pointer"
+          onMouseEnter={() => setHoveredProject("Upendo")}
+          onMouseLeave={() => setHoveredProject("Kies project")}
+          onClick={() => handlePolygonClick("/projects/mcdonalds")} // Navigatie bij klik
+          style={{
+            transform: "scale(1.4)",
+            backgroundImage: "url('/images/Upendo.png')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            WebkitMaskImage: "url('/polygons/Polygon1.svg')",
+            WebkitMaskSize: "contain",
+            WebkitMaskRepeat: "no-repeat",
+            maskImage: "url('/polygons/Polygon1.svg')",
+            maskSize: "contain",
+            maskRepeat: "no-repeat",
+          }}
+          onMouseOver={(e) =>
+            (e.currentTarget.style.transform = "scale(1.5) translateZ(20px)")
+          }
+          onMouseOut={(e) =>
+            (e.currentTarget.style.transform = "scale(1.4) translateZ(0px)")
+          }
+        ></div>
 
-          {/* Nieuwe polygon - Linksboven, -55 graden gedraaid */}
-          <div
-            className="absolute w-full h-full mask-polygon"
-            style={{
-              transform: "translate(-50%, -50%) rotate(73deg) scale(1.4)",
-              top: "-10%",
-              left: "-40%",
-              backgroundImage: "url('/images/Trendfocus.svg')",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              WebkitMaskImage: "url('/polygons/Polygon1.svg')",
-              WebkitMaskSize: "contain",
-              WebkitMaskRepeat: "no-repeat",
-              maskImage: "url('/polygons/Polygon1.svg')",
-              maskSize: "contain",
-              maskRepeat: "no-repeat",
-            }}
-          ></div>
+        {/* Nieuwe polygon - Rechtsboven */}
+        <div
+          className="absolute w-full h-full mask-polygon transition-transform duration-300 cursor-pointer"
+          onMouseEnter={() => setHoveredProject("TrendFocus")}
+          onMouseLeave={() => setHoveredProject("Kies project")}
+          onClick={() => handlePolygonClick("/projects/upendo")}
+          style={{
+            transform: "translate(50%, -50%) rotate(-73deg) scale(1.4)",
+            top: "-25%",
+            right: "-75%",
+            backgroundImage: "url('/images/Trendfocus.svg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            WebkitMaskImage: "url('/polygons/Polygon1.svg')",
+            WebkitMaskSize: "contain",
+            WebkitMaskRepeat: "no-repeat",
+            maskImage: "url('/polygons/Polygon1.svg')",
+            maskSize: "contain",
+            maskRepeat: "no-repeat",
+          }}
+          onMouseOver={(e) =>
+            (e.currentTarget.style.transform =
+              "translate(50%, -50%) rotate(-73deg) scale(1.5) translateZ(20px)")
+          }
+          onMouseOut={(e) =>
+            (e.currentTarget.style.transform =
+              "translate(50%, -50%) rotate(-73deg) scale(1.4) translateZ(0px)")
+          }
+        ></div>
 
-          {/* Extra polygon - Boven rechtsboven */}
-          <div
-            className="absolute w-full h-full mask-polygon"
-            style={{
-              transform: "translate(50%, -100%) rotate(220deg) scale(1.4)",
-              top: "-65%",
-              right: "-10%",
-              backgroundImage: "url('/images/mcdonald.svg')",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              WebkitMaskImage: "url('/polygons/Polygon1.svg')",
-              WebkitMaskSize: "contain",
-              WebkitMaskRepeat: "no-repeat",
-              maskImage: "url('/polygons/Polygon1.svg')",
-              maskSize: "contain",
-              maskRepeat: "no-repeat",
-            }}
-          ></div>
+        {/* Nieuwe polygon - Linksboven */}
+        <div
+          className="absolute w-full h-full mask-polygon transition-transform duration-300 cursor-pointer"
+          onMouseEnter={() => setHoveredProject("Trendfocus")}
+          onMouseLeave={() => setHoveredProject("Kies project")}
+          onClick={() => handlePolygonClick("/projects/trendfocus")}
+          style={{
+            transform: "translate(-50%, -50%) rotate(73deg) scale(1.4)",
+            top: "-25%",
+            left: "-75%",
+            backgroundImage: "url('/images/Trendfocus.svg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            WebkitMaskImage: "url('/polygons/Polygon1.svg')",
+            WebkitMaskSize: "contain",
+            WebkitMaskRepeat: "no-repeat",
+            maskImage: "url('/polygons/Polygon1.svg')",
+            maskSize: "contain",
+            maskRepeat: "no-repeat",
+          }}
+          onMouseOver={(e) =>
+            (e.currentTarget.style.transform =
+              "translate(-50%, -50%) rotate(73deg) scale(1.5) translateZ(20px)")
+          }
+          onMouseOut={(e) =>
+            (e.currentTarget.style.transform =
+              "translate(-50%, -50%) rotate(73deg) scale(1.4) translateZ(0px)")
+          }
+        ></div>
 
-          {/* Extra polygon - Boven linksboven */}
-          <div
-            className="absolute w-full h-full mask-polygon"
-            style={{
-              transform: "translate(-50%, -100%) rotate(-220deg) scale(1.4)",
-              top: "-65%",
-              left: "-10%",
-              backgroundImage: "url('/images/Upendo.png')",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              WebkitMaskImage: "url('/polygons/Polygon1.svg')",
-              WebkitMaskSize: "contain",
-              WebkitMaskRepeat: "no-repeat",
-              maskImage: "url('/polygons/Polygon1.svg')",
-              maskSize: "contain",
-              maskRepeat: "no-repeat",
-            }}
-          ></div>
-        </div>
+        {/* Extra polygon - Boven rechtsboven */}
+        <div
+          className="absolute w-full h-full mask-polygon transition-transform duration-300 cursor-pointer"
+          onMouseEnter={() => setHoveredProject("Coming Soon")}
+          onMouseLeave={() => setHoveredProject("Kies project")}
+          onClick={() => handlePolygonClick("/projects/extra-rechts")}
+          style={{
+            transform: "translate(50%, -100%) rotate(220deg) scale(1.4)",
+            top: "-100%",
+            right: "-35%",
+            backgroundImage: "url('/images/mcdonald.svg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            WebkitMaskImage: "url('/polygons/Polygon1.svg')",
+            WebkitMaskSize: "contain",
+            WebkitMaskRepeat: "no-repeat",
+            maskImage: "url('/polygons/Polygon1.svg')",
+            maskSize: "contain",
+            maskRepeat: "no-repeat",
+          }}
+          onMouseOver={(e) =>
+            (e.currentTarget.style.transform =
+              "translate(50%, -100%) rotate(220deg) scale(1.5) translateZ(20px)")
+          }
+          onMouseOut={(e) =>
+            (e.currentTarget.style.transform =
+              "translate(50%, -100%) rotate(220deg) scale(1.4) translateZ(0px)")
+          }
+        ></div>
+
+        {/* Extra polygon - Boven linksboven */}
+        <div
+          className="absolute w-full h-full mask-polygon transition-transform duration-300 cursor-pointer"
+          onMouseEnter={() => setHoveredProject("Upendo")}
+          onMouseLeave={() => setHoveredProject("Kies project")}
+          onClick={() => handlePolygonClick("/projects/extra-links")}
+          style={{
+            transform: "translate(-50%, -100%) rotate(-220deg) scale(1.4)",
+            top: "-100%",
+            left: "-35%",
+            backgroundImage: "url('/images/Upendo.png')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            WebkitMaskImage: "url('/polygons/Polygon1.svg')",
+            WebkitMaskSize: "contain",
+            WebkitMaskRepeat: "no-repeat",
+            maskImage: "url('/polygons/Polygon1.svg')",
+            maskSize: "contain",
+            maskRepeat: "no-repeat",
+          }}
+          onMouseOver={(e) =>
+            (e.currentTarget.style.transform =
+              "translate(-50%, -100%) rotate(-220deg) scale(1.5) translateZ(20px)")
+          }
+          onMouseOut={(e) =>
+            (e.currentTarget.style.transform =
+              "translate(-50%, -100%) rotate(-220deg) scale(1.4) translateZ(0px)")
+          }
+        ></div>
       </div>
     </div>
   );

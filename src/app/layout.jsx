8541cbb6e7fea6306f1bc90817d98fc1c98ad03e
Yusuf013 <146951398+Yusuf013@users.noreply.github.com>
2025-01-03 21:17:52 +0100

    contact button animation added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -22,56 +22,44 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-      
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <nav className="flex items-center justify-center p-3 bg-[#283127]">
-        <div className="w-[10%] flex items-center justify-around">
-        <Image
-          src="/images/LinkedIn (1).svg"
-          alt="Logo"
-          width={33}
-          height={38} 
-        />
-           <Image
-          src="/images/GitHub.svg"
-          alt="Logo"
-          width={33}
-          height={38} 
-        />
-           <Image
-          src="/images/Instagram.svg"
-          alt="Logo"
-          width={33}
-          height={38} 
-        />
-        </div>
-        <div className="w-[75%]  flex items-center justify-center ">
-          <ul className="flex justify-center w-[100%] text-2xl text-[#F47519]">
-            <li>
-            <Link href="/">
-                Home
-              </Link>
-              </li>
-            <li className="mx-32">
-              <Link href="/aboutme">
-                About me
-              </Link>
-            </li>
-            <li>
-              <Link href="/projects">
-                Projects
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="w-[10%]  flex items-center justify-center">
-          <button className="rounded-full bg-[#F47519] text-[#283127] font-bold px-3 py-1">Contact</button>
+          <div className="w-[10%] flex items-center justify-around">
+            <Image
+              src="/images/LinkedIn (1).svg"
+              alt="Logo"
+              width={33}
+              height={38}
+            />
+            <Image src="/images/GitHub.svg" alt="Logo" width={33} height={38} />
+            <Image
+              src="/images/Instagram.svg"
+              alt="Logo"
+              width={33}
+              height={38}
+            />
           </div>
-      </nav>
-        <div className="background">
-          {children}
-        </div>
+          <div className="w-[75%]  flex items-center justify-center ">
+            <ul className="flex justify-center w-[100%] text-2xl text-[#F47519]">
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li className="mx-32">
+                <Link href="/aboutme">About me</Link>
+              </li>
+              <li>
+                <Link href="/projects">Projects</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="w-[10%] flex items-center justify-center">
+            <button className="rounded-full bg-[#F47519] text-[#283127] font-bold px-3 py-1 hover:bg-[#FFFDE4] transition-colors duration-300">
+              Contact
+            </button>
+          </div>
+        </nav>
+        <div className="background">{children}</div>
       </body>
     </html>
   );

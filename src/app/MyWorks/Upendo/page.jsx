652fc66971275129de22e3d98c98755adfbc033b
Yusuf013 <146951398+Yusuf013@users.noreply.github.com>
2025-01-05 20:40:08 +0100

    project page almost done
    added a extra section to display mobile designs

--- src/app/MyWorks/Upendo/page.jsx
+++ src/app/MyWorks/Upendo/page.jsx
@@ -13,6 +13,8 @@ const UpendoPage = () => {
     '/images/UpendoS1.png',
     '/images/UpendoS2.png',
     '/images/UpendoS3.png',
+    '/images/UpendoS4.png',
+    '/images/UpendoS5.png',
   ]
 
   useEffect(() => {
@@ -73,7 +75,7 @@ const UpendoPage = () => {
     setImageOpacity(0) // Start met fade-out en blur
     const timeout = setTimeout(() => {
       setImageOpacity(1) // Fade-in en verwijder blur
-    }, 500) // Duur van de overgang
+    }, 300) // Duur van de overgang
 
     return () => clearTimeout(timeout)
   }, [currentImageIndex])
@@ -185,9 +187,8 @@ const UpendoPage = () => {
 
         {/* section 3 */}
         <section
-          className="w-screen h-full flex-shrink-0 flex items-center justify-center bg-[#283127] mt-10 p-0 relative"
+          className="w-screen h-full flex items-center justify-center bg-[#283127] mt-10 p-0 relative"
           data-scroll-section
-          data-scroll-id="section3"
         >
           <div
             className="absolute inset-0 bg-no-repeat bg-cover bg-center"
@@ -195,8 +196,47 @@ const UpendoPage = () => {
               backgroundImage: "url('/images/sectie3bg.svg')",
             }}
           ></div>
+          <div className="relative z-10 flex justify-center flex-col items-center">
+            <h2 className="text-[#FFFDE4] font-formula1 text-[40px] mb-5">Design</h2>
+            <div className="w-[85%] flex justify-around">
+              <img
+                src="/images/UpendoM1.PNG"
+                className="w-auto h-[400px]"
+                alt="Upendo Image 1"
+              />
+              <img
+                src="/images/UpendoM2.PNG"
+                className="w-auto h-[400px]"
+                alt="Upendo Image 2"
+              />
+              <img
+                src="/images/UpendoM3.PNG"
+                className="w-auto h-[400px]"
+                alt="Upendo Image 3"
+              />
+              <img
+                src="/images/UpendoM4.PNG"
+                className="w-auto h-[400px]"
+                alt="Upendo Image 4"
+              />
+            </div>
+          </div>
+        </section>
+
+        {/* section 4 */}
+        <section
+          className="w-screen h-full flex-shrink-0 flex items-center justify-center bg-[#283127] mt-10 p-0 relative"
+          data-scroll-section
+          data-scroll-id="section3"
+        >
+          <div
+            className="absolute inset-0 bg-no-repeat bg-cover bg-center"
+            style={{
+              backgroundImage: "url('/images/sectie4bg.svg')",
+            }}
+          ></div>
           <div className="relative z-10 absolute w-full h-full flex items-center justify-center">
-            <div className="w-[75%] h-[55%] bg-[#283127] overflow-hidden flex items-center justify-center mb-64">
+            <div className="w-[75%] h-[60%] bg-[#283127] overflow-hidden flex items-center justify-center mb-64">
               <img
                 src={images[currentImageIndex]}
                 alt={`Image ${currentImageIndex + 1}`}

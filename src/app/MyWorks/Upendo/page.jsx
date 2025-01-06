fb9f627fbd0582e98374cc3cefcfc0e81eb8e615
Yusuf013 <146951398+Yusuf013@users.noreply.github.com>
2025-01-06 15:32:30 +0100

    added extra picture to Upendo page

--- src/app/MyWorks/Upendo/page.jsx
+++ src/app/MyWorks/Upendo/page.jsx
@@ -212,6 +212,11 @@ const UpendoPage = () => {
                 alt="Upendo Image 1"
               />
               <img
+                src="/images/UpendoM2.PNG"
+                className="w-auto h-[400px]"
+                alt="Upendo Image 2"
+              />
+              <img
                 src="/images/UpendoM3.PNG"
                 className="w-auto h-[400px]"
                 alt="Upendo Image 3"
